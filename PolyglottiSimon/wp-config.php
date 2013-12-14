4448157a30c5ea50d4503b0f04d4b1ba9797b5c7
SimonTalaga <simon.talaga@gmail.com>
2013-12-14 16:13:20 +0100

    Import fonctionnel mais pas encore dans bdd

---
+++ PolyglottiSimon/wp-config.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * La configuration de base de votre installation WordPress.
+ *
+ * Ce fichier contient les réglages de configuration suivants : réglages MySQL,
+ * préfixe de table, clefs secrètes, langue utilisée, et ABSPATH.
+ * Vous pouvez en savoir plus à leur sujet en allant sur
+ * {@link http://codex.wordpress.org/fr:Modifier_wp-config.php Modifier
+ * wp-config.php}. C'est votre hébergeur qui doit vous donner vos
+ * codes MySQL.
+ *
+ * Ce fichier est utilisé par le script de création de wp-config.php pendant
+ * le processus d'installation. Vous n'avez pas à utiliser le site web, vous
+ * pouvez simplement renommer ce fichier en "wp-config.php" et remplir les
+ * valeurs.
+ *
+ * @package WordPress
+ */
+
+// ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
+/** Nom de la base de données de WordPress. */
+define('DB_NAME', 'webmrl13');
+/** Utilisateur de la base de données MySQL. */
+define('DB_USER', 'webmrl13');
+
+/** Mot de passe de la base de données MySQL. */
+define('DB_PASSWORD', 'polyglotte2013');
+
+/** Adresse de l'hébergement MySQL. */
+define('DB_HOST', 'localhost');
+
+/** Jeu de caractères à utiliser par la base de données lors de la création des tables. */
+define('DB_CHARSET', 'utf8');
+
+/** Type de collation de la base de données.
+  * N'y touchez que si vous savez ce que vous faites.
+  */
+define('DB_COLLATE', '');
+
+/**#@+
+ * Clefs uniques d'authentification et salage.
+ *
+ * Remplacez les valeurs par défaut par des phrases uniques !
+ * Vous pouvez générer des phrases aléatoires en utilisant
+ * {@link https://api.wordpress.org/secret-key/1.1/salt/ le service de clefs secrètes de WordPress.org}.
+ * Vous pouvez modifier ces phrases à n'importe quel moment, afin d'invalider tous les cookies existants.
+ * Cela forcera également tous les utilisateurs à se reconnecter.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         '3g+(,%>S+`*P]gq=j[u_4D@Z11Aj}(%nF[B(d&XU:OAJHorT0=Q94k!q3Fq]5)}_');
+define('SECURE_AUTH_KEY',  '3DEsOd^3:wOX*Au 7lSv>e2[DfBGSM4Or=.R*7O`/?,M]5a!mhGBC#5.+l+W^NEt');
+define('LOGGED_IN_KEY',    'gpAZ)HKRR{*WIur0fVsi`/]$(,T2,aTUe$oNfxmM@;-ZR=28zZ=TSt!-b%AuH#|^');
+define('NONCE_KEY',        'I#$3k@mJ&25+=m78s->qShtNLgZji?cW2O/l[CQ%zx&!DgT1&?Vr,?5#**iIhCM]');
+define('AUTH_SALT',        '=W *Ri8W45u=RIx[wBO=STsG$g-k${JLq)|Pr2%ij%i5n=OYgN^X,!!d.>Bm mm+');
+define('SECURE_AUTH_SALT', '_f$d<v/g4Zr%;sD:z^V`W*n;ctxxrIv6_R.I>pAz6Wb)1>A^&-[}KoAE=Q2V~gi&');
+define('LOGGED_IN_SALT',   '=f`3SD d,p(&BNh[NI^.|CUKB.tVf%XuW&CsQ:/ub<_|Flv7{Ytd$Ze& 5 wvd<#');
+define('NONCE_SALT',       'RI|b-P;5dgjV|CY]HM R^@uu  w.pEG}z&w4HN=fJ=?-gR>,n]}j|d_aWjd49*Uu');
+/**#@-*/
+
+/**
+ * Préfixe de base de données pour les tables de WordPress.
+ *
+ * Vous pouvez installer plusieurs WordPress sur une seule base de données
+ * si vous leur donnez chacune un préfixe unique.
+ * N'utilisez que des chiffres, des lettres non-accentuées, et des caractères soulignés!
+ */
+$table_prefix  = 'wp_';
+
+/**
+ * Langue de localisation de WordPress, par défaut en Anglais.
+ *
+ * Modifiez cette valeur pour localiser WordPress. Un fichier MO correspondant
+ * au langage choisi doit être installé dans le dossier wp-content/languages.
+ * Par exemple, pour mettre en place une traduction française, mettez le fichier
+ * fr_FR.mo dans wp-content/languages, et réglez l'option ci-dessous à "fr_FR".
+ */
+define('WPLANG', 'fr_FR');
+
+/**
+ * Pour les développeurs : le mode deboguage de WordPress.
+ *
+ * En passant la valeur suivante à "true", vous activez l'affichage des
+ * notifications d'erreurs pendant votre essais.
+ * Il est fortemment recommandé que les développeurs d'extensions et
+ * de thèmes se servent de WP_DEBUG dans leur environnement de
+ * développement.
+ */
+define('WP_DEBUG', false);
+
+/* C'est tout, ne touchez pas à ce qui suit ! Bon blogging ! */
+
+/** Chemin absolu vers le dossier de WordPress. */
+if ( !defined('ABSPATH') )
+  define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Réglage des variables de WordPress et de ses fichiers inclus. */
+require_once(ABSPATH . 'wp-settings.php');
---
+++ PolyglottiSimon/wp-content/themes/polyglotti/functions.php
@@ -0,0 +1,234 @@
+<?php
+
+// Get the lesson pannel on admin interface
+require 'lesson-pannel.php';
+
+//require_once (ABSPATH . 'wp-content/themes/framework/theme.php');
+
+//$theme = new Theme(array(
+//    'name' => 'Polyglotti',
+//    'slug' => 'polyglotti',
+//    'options' => array(
+//        array(
+//            'name' => 'phrases',
+//            'slug' => 'lessons',
+//            'pages'=> array(
+//                'Generale'=>'general'
+//            )
+//        )
+//    )
+//));
+
+
+// Enable thumbnails
+add_theme_support( 'post-thumbnails' );
+set_post_thumbnail_size(200, 200, true); // Normal post thumbnails
+
+
+
+// Remove the admin bar from the front end
+add_filter( 'show_admin_bar', '__return_false' );
+
+
+// Customise the footer in admin area
+function wpfme_footer_admin () {
+    echo 'Theme designed and developed by <a href="#" target="_blank">MMI 2013</a>.';
+}
+add_filter('admin_footer_text', 'wpfme_footer_admin');
+
+
+// Set a maximum width for Oembedded objects
+if ( ! isset( $content_width ) )
+    $content_width = 660;
+
+
+// Add default posts and comments RSS feed links to head
+add_theme_support( 'automatic-feed-links' );
+
+
+// Put post thumbnails into rss feed
+function wpfme_feed_post_thumbnail($content) {
+    global $post;
+    if(has_post_thumbnail($post->ID)) {
+        $content = '' . $content;
+    }
+    return $content;
+}
+add_filter('the_excerpt_rss', 'wpfme_feed_post_thumbnail');
+add_filter('the_content_feed', 'wpfme_feed_post_thumbnail');
+
+
+// Add custom menus
+register_nav_menus( array(
+    'primary' => __( 'Primary Navigation', 'wpfme' ),
+    //'example' => __( 'Example Navigation', 'wpfme' ),
+) );
+
+
+// Randomly chosen placeholder text for post/page edit screen
+function wpfme_writing_encouragement( $content ) {
+    global $post_type;
+    if($post_type == "post"){
+        $encArray = array(
+            // Placeholders for the posts editor
+            "Test post message one.",
+            "Test post message two.",
+            "<h1>Test post heading!</h1>"
+        );
+        return $encArray[array_rand($encArray)];
+    }
+    else{ $encArray = array(
+        // Placeholders for the pages editor
+        "Test page message one.",
+        "Test page message two.",
+        "<h1>Test Page Heading</h1>"
+    );
+        return $encArray[array_rand($encArray)];
+    }
+}
+add_filter( 'default_content', 'wpfme_writing_encouragement' );
+
+
+//change amount of posts on the search page
+function wpfme_search_results_per_page( $query ) {
+    global $wp_the_query;
+    if ( ( ! is_admin() ) && ( $query === $wp_the_query ) && ( $query->is_search() ) ) {
+        $query->set( 'wpfme_search_results_per_page', 100 );
+    }
+    return $query;
+}
+add_action( 'pre_get_posts',  'wpfme_search_results_per_page'  );
+
+
+//create a permalink after the excerpt
+function wpfme_replace_excerpt($content) {
+    return str_replace('[...]',
+        '<a class="readmore" href="'. get_permalink() .'">suite...</a>',
+        $content
+    );
+}
+add_filter('the_excerpt', 'wpfme_replace_excerpt');
+
+
+function wpfme_has_sidebar($classes) {
+    if (is_active_sidebar('sidebar')) {
+        // add 'class-name' to the $classes array
+        $classes[] = 'has_sidebar';
+    }
+    // return the $classes array
+    return $classes;
+}
+add_filter('body_class','wpfme_has_sidebar');
+
+
+// Create custom sizes
+// This is then pulled through to your theme useing the_post_thumbnail('custombig');
+if ( function_exists( 'add_image_size' ) ) {
+    add_image_size('customsmall', 300, 200, true); //narrow column
+    add_image_size('custombig', 400, 500, true); //wide column
+}
+
+
+// Stop images getting wrapped up in p tags when they get dumped out with the_content() for easier theme styling
+function wpfme_remove_img_ptags($content){
+    return preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
+}
+add_filter('the_content', 'wpfme_remove_img_ptags');
+
+
+// Call the google CDN version of jQuery for the frontend
+// Make sure you use this with wp_enqueue_script('jquery'); in your header
+function wpfme_jquery_enqueue() {
+    wp_deregister_script('jquery');
+    wp_register_script('jquery', "http" . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . "://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js", false, null);
+    wp_enqueue_script('jquery');
+}
+if (!is_admin()) add_action("wp_enqueue_scripts", "wpfme_jquery_enqueue", 11);
+
+
+//custom excerpt length
+function wpfme_custom_excerpt_length( $length ) {
+    //the amount of words to return
+    return 20;
+}
+add_filter( 'excerpt_length', 'wpfme_custom_excerpt_length');
+
+
+// Call Googles HTML5 Shim, but only for users on old versions of IE
+function wpfme_IEhtml5_shim () {
+    global $is_IE;
+    if ($is_IE)
+        echo '<!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->';
+}
+add_action('wp_head', 'wpfme_IEhtml5_shim');
+
+
+// Disable the theme / plugin text editor in Admin
+define('DISALLOW_FILE_EDIT', true);
+
+// Register Custom Post Type
+function phrases() {
+
+    $labels = array(
+        'name'                => _x( 'Phrases', 'Post Type General Name', 'text_domain' ),
+        'singular_name'       => _x( 'Phrase', 'Post Type Singular Name', 'text_domain' ),
+        'menu_name'           => __( 'Phrases', 'text_domain' ),
+        'parent_item_colon'   => __( '', 'text_domain' ),
+        'all_items'           => __( 'Toutes les phrases', 'text_domain' ),
+        'view_item'           => __( '', 'text_domain' ),
+        'add_new_item'        => __( 'Ajouter une nouvelle phrase', 'text_domain' ),
+        'add_new'             => __( 'Nouvelle phrase', 'text_domain' ),
+        'edit_item'           => __( 'Editer phrase', 'text_domain' ),
+        'update_item'         => __( 'Mettre à jour phrase', 'text_domain' ),
+        'search_items'        => __( 'Rechercher dans les phrases', 'text_domain' ),
+        'not_found'           => __( 'Aucune phrase trouvée', 'text_domain' ),
+        'not_found_in_trash'  => __( 'No phrases found in Trash', 'text_domain' ),
+    );
+
+    $rewrite = array(
+        'slug'                => 'Phrases',
+        'with_front'          => true,
+        'pages'               => true,
+        'feeds'               => true,
+    );
+    $args = array(
+        'label'               => __( 'phrases', 'text_domain' ),
+        'description'         => __( 'Phrase information pages', 'text_domain' ),
+        'labels'              => $labels,
+        'supports'            => array('title', 'custom-fields'),
+        'taxonomies'          => array(),
+        'hierarchical'        => false,
+        'public'              => false,
+        'show_ui'             => true,
+        'show_in_menu'        => true,
+        'show_in_nav_menus'   => true,
+        'show_in_admin_bar'   => true,
+        'menu_position'       => 5,
+        'menu_icon'           => '',
+        'can_export'          => true,
+        'has_archive'         => false,
+        'exclude_from_search' => false,
+        'publicly_queryable'  => true,
+        'rewrite'             => $rewrite,
+        'capability_type'     => 'page',
+    );
+    register_post_type( 'phrases', $args );
+
+}
+
+// Hook into the 'init' action
+add_action( 'init', 'phrases', 0 );
+
+// function wpc_champs_personnalises_defaut($post_id)
+// {
+//     if ($_GET['post_type'] == 'phrases') {
+//         add_post_meta($post_id, 'Numéro de leçon', '', true);
+//         add_post_meta($post_id, 'Chinois', '', true);
+//         add_post_meta($post_id, 'Chinois - Pinyin', '', true);
+//         add_post_meta($post_id, 'Anglais', '', true);
+//     }
+//     return true;
+//  }
+
+// add_action('wp_insert_post', 'wpc_champs_personnalises_defaut');
+
